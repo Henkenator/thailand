98120cd2ca76406c442e25a2e78d494f92f4ae0e
Henkenator <ljunggren.henrik@gmail.com>
2019-01-24 17:50:11 +0100

    added pic carousel for area

--- src/app/components/area/area.component.ts
+++ src/app/components/area/area.component.ts
@@ -12,7 +12,25 @@ export class AreaComponent implements OnInit {
   ngOnInit() {
   }
 
-  areaImages = [
+  mangoImages = [
+
+    { text: 'Pool', src: '../../../assets/images/area/pool2.jpg'},
+    { text: 'Pool', src: '../../../assets/images/area/pool1.jpg'},
+
+    { text: 'Området', src: '../../../assets/images/area/omradet1.jpg'},
+    { text: 'Området', src: '../../../assets/images/area/omradet2.jpg'},
+
+    { text: 'Restaurang', src: '../../../assets/images/area/restaurang.jpg'},
+
+    { text: 'Strand', src: '../../../assets/images/area/strand1.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/strand2.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/strand3.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/strand4.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/strand5.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/strand6.jpg'},
+  ];
+
+  phimImages = [
 
     { text: 'Pool', src: '../../../assets/images/area/pool2.jpg'},
     { text: 'Pool', src: '../../../assets/images/area/pool1.jpg'},
