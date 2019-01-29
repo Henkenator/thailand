ff4fff168581b6a6fea30293d790b485fa9a016f
Henkenator <ljunggren.henrik@gmail.com>
2019-01-29 23:27:23 +0100

    mail form func

--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,39 +1,11 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-booking',
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.css']
 })
-export class BookingComponent implements OnInit {
+export class BookingComponent {
 
-  bookingForm: FormGroup;
-  feedbackText: string;
-  // feedbackText = 'Tack för din förfrågan! Vi återkommer så snart som möjligt med svar.';
-
-  constructor(private fb: FormBuilder) {
-  }
-
-  ngOnInit() {
-    this.createForm();
-  }
-
-  createForm() {
-    this.bookingForm = this.fb.group({
-      name: ['', Validators.required],
-      address: ['', Validators.required],
-      zipCode: ['', Validators.required],
-      city: ['', Validators.required],
-      phone: [''],
-      email: ['', Validators.required],
-      message: ['', Validators.required],
-    });
-  }
-
-  onSubmit() {
-    console.log(this.bookingForm.value);
-    this.createForm();
-    this.feedbackText = 'Tack för din förfrågan! Vi återkommer så snart som möjligt med svar.';
-  }
+  constructor() {}
 }
