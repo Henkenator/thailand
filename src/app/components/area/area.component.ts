93b6969da9efb9af34f112636fb9007d64b4b60f
Henkenator <ljunggren.henrik@gmail.com>
2019-02-01 00:48:55 +0100

    added images

--- src/app/components/area/area.component.ts
+++ src/app/components/area/area.component.ts
@@ -14,38 +14,51 @@ export class AreaComponent implements OnInit {
 
   mangoImages = [
 
-    { text: 'Pool', src: '../../../assets/images/area/pool2.jpg'},
-    { text: 'Pool', src: '../../../assets/images/area/pool1.jpg'},
+    { text: 'Områdets entré', src: '../../../assets/images/area/blueMango/entre.jpg'},
+    { text: 'Vaktkur och port', src: '../../../assets/images/area/blueMango/port.jpg'},
 
-    { text: 'Området', src: '../../../assets/images/area/omradet1.jpg'},
-    { text: 'Området', src: '../../../assets/images/area/omradet2.jpg'},
+    { text: 'Området', src: '../../../assets/images/area/blueMango/hus1.jpg'},
+    { text: 'Området', src: '../../../assets/images/area/blueMango/hus2.jpg'},
+    { text: 'Området', src: '../../../assets/images/area/blueMango/hus3.jpg'},
+    { text: 'Området', src: '../../../assets/images/area/blueMango/hus4.jpg'},
+    { text: 'Området', src: '../../../assets/images/area/blueMango/hus5.jpg'},
 
-    { text: 'Restaurang', src: '../../../assets/images/area/restaurang.jpg'},
+    { text: 'Pool 1', src: '../../../assets/images/area/blueMango/pool1.jpg'},
+    { text: 'Pool 2', src: '../../../assets/images/area/blueMango/pool2.jpg'},
 
-    { text: 'Strand', src: '../../../assets/images/area/strand1.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand2.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand3.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand4.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand5.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand6.jpg'},
+    { text: 'Entré kvällstid', src: '../../../assets/images/area/blueMango/nattentre.jpg'},
+    { text: 'Området på kvällen', src: '../../../assets/images/area/blueMango/natt-hus.jpg'},
+    { text: 'Pool 1 på kvällen', src: '../../../assets/images/area/blueMango/natt-pool1-1.jpg'},
+    { text: 'Pool 1 på kvällen', src: '../../../assets/images/area/blueMango/natt-pool1-2.jpg'},
+    { text: 'Pool 2 på kvällen', src: '../../../assets/images/area/blueMango/natt-pool2.jpg'}
   ];
 
   phimImages = [
 
-    { text: 'Pool', src: '../../../assets/images/area/pool2.jpg'},
-    { text: 'Pool', src: '../../../assets/images/area/pool1.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/maePhim/strand1.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/maePhim/strand2.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/maePhim/strand3.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/maePhim/strand4.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/maePhim/strand5.jpg'},
+    { text: 'Strand', src: '../../../assets/images/area/maePhim/strand6.jpg'},
 
-    { text: 'Området', src: '../../../assets/images/area/omradet1.jpg'},
-    { text: 'Området', src: '../../../assets/images/area/omradet2.jpg'},
+    { text: 'Mae Phim', src: '../../../assets/images/area/maePhim/maePhim1.jpg'},
+    { text: 'Mae Phim', src: '../../../assets/images/area/maePhim/maePhim2.jpg'},
+    { text: 'Mae Phim', src: '../../../assets/images/area/maePhim/maePhim3.jpg'},
+    { text: 'Mae Phim', src: '../../../assets/images/area/maePhim/maePhim4.jpg'},
 
-    { text: 'Restaurang', src: '../../../assets/images/area/restaurang.jpg'},
+    { text: 'Torget i Mae Phim', src: '../../../assets/images/area/maePhim/torg1.jpg'},
+    { text: 'Torget i Mae Phim', src: '../../../assets/images/area/maePhim/torg2.jpg'},
 
-    { text: 'Strand', src: '../../../assets/images/area/strand1.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand2.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand3.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand4.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand5.jpg'},
-    { text: 'Strand', src: '../../../assets/images/area/strand6.jpg'},
+    { text: 'En stormig dag med högt vatten', src: '../../../assets/images/area/maePhim/storm.jpg'},
+
+    { text: 'Mat', src: '../../../assets/images/area/maePhim/mat1.jpg'},
+    { text: 'Mat', src: '../../../assets/images/area/maePhim/mat2.jpg'},
+    { text: 'Mat', src: '../../../assets/images/area/maePhim/mat3.jpg'},
+    { text: 'Mat', src: '../../../assets/images/area/maePhim/mat4.jpg'},
+    { text: 'Mat', src: '../../../assets/images/area/maePhim/mat5.jpg'},
+    { text: 'Mat', src: '../../../assets/images/area/maePhim/mat6.jpg'},
+    // { text: 'Mat', src: '../../../assets/images/area/maePhim/mat7.jpg'},
   ];
 
 }
--- src/app/components/house/house.component.ts
+++ src/app/components/house/house.component.ts
@@ -11,38 +11,35 @@ export class HouseComponent implements OnInit {
 
     { text: 'Vardagsrum', src: '../../../assets/images/house/vardagsrum1.jpg'},
     { text: 'Vardagsrum', src: '../../../assets/images/house/vardagsrum2.jpg'},
+    { text: 'Vardagsrum ut mot altan', src: '../../../assets/images/house/vardagsrum3.jpg'},
 
-    { text: 'Matsal', src: '../../../assets/images/house/matsal1.jpg'},
-    { text: 'Matsal', src: '../../../assets/images/house/matsal2.jpg'},
+    { text: 'Bord med plats för 8', src: '../../../assets/images/house/matsal1.jpg'},
+    { text: 'Bord med plats för 8', src: '../../../assets/images/house/matsal2.jpg'},
 
     { text: 'Kök', src: '../../../assets/images/house/kok.jpg'},
 
-    { text: 'Sovrum 1', src: '../../../assets/images/house/sovrum3.jpg'},
+    { text: 'Sovrum 1', src: '../../../assets/images/house/sovrum1.jpg'},
     { text: 'Sovrum 2', src: '../../../assets/images/house/sovrum2.jpg'},
-    { text: 'Sovrum 2', src: '../../../assets/images/house/sovrum4.jpg'},
+    { text: 'Sovrum 2', src: '../../../assets/images/house/sovrum3.jpg'},
 
     { text: 'Tv-rum', src: '../../../assets/images/house/tv-rum1.jpg'},
-    { text: 'Tv-rum', src: '../../../assets/images/house/tv-rum2.jpg'},
+    { text: 'Fåtöljer/gästsängar', src: '../../../assets/images/house/tv-rum2.jpg'},
 
   ];
 
   outdoorImages = [
 
-    { text: 'Hus', src: '../../../assets/images/house/hus1.jpg'},
-    { text: 'Hus', src: '../../../assets/images/house/hus2.jpg'},
-    { text: 'Hus', src: '../../../assets/images/house/hus3.jpg'},
+    { text: 'Huset', src: '../../../assets/images/house/hus1.jpg'},
+    { text: 'Huset sett från poolen', src: '../../../assets/images/house/hus2.jpg'},
+
+    { text: 'Balkong', src: '../../../assets/images/house/balkong1.jpg'},
+    { text: 'Balkong', src: '../../../assets/images/house/balkong2.jpg'},
+    { text: 'Utsikt från balkong', src: '../../../assets/images/house/balkong3.jpg'},
+
 
     { text: 'Altan', src: '../../../assets/images/house/altan1.jpg'},
     { text: 'Altan', src: '../../../assets/images/house/altan2.jpg'},
-    { text: 'Altan', src: '../../../assets/images/house/altan3.jpg'},
-    { text: 'Altan', src: '../../../assets/images/house/altan4.jpg'},
-
-    { text: 'Pool', src: '../../../assets/images/house/pool1.jpg'},
-    { text: 'Pool', src: '../../../assets/images/house/pool2.jpg'},
-
-    { text: 'Terass', src: '../../../assets/images/house/terass1.jpg'},
-    { text: 'Terass', src: '../../../assets/images/house/terass2.jpg'},
-    { text: 'Terass', src: '../../../assets/images/house/terass3.jpg'},
+    { text: '2 st cyklar', src: '../../../assets/images/house/cyklar.jpg'},
 
    ];
 
